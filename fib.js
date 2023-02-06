b2103613d274a57a1678027a598ac43755e5eee5
OsamaHashmi <osamahashmi@osama>
2023-02-06 21:13:14 +0500

    corrent fib version using memo

--- fib.js
+++ fib.js
@@ -1,6 +1,27 @@
+
+
+//this is basic funtion which is use to calculate fib number 
+//there is an issue 
+//-->if we try to increase the n number then it will increase the time complixity of program.
+
+/*
 const fib = (n) => {
     if (n <= 2) return 1;
     console.log(n);
     return fib(n - 1) + fib(n - 2);
 };
 console.log(fib(7));
+*/
+//---------------------------------------------------------------------//
+//TO fix this issue 
+// We will implement memoization in which we store the subproblems in something to remove dublicaton.
+//i will use javascript object in Javascript
+
+const fib = (n,memo={}) => {
+    if (n in memo)return memo[n];
+    if (n <= 2) return 1;
+    // console.log(n);
+    memo[n]= fib(n - 1,memo) + fib(n - 2,memo);
+    return memo[n];
+};
+console.log(fib(6));
